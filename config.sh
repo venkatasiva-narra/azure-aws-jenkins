c985e4fe6bfdff7e07b1cc9072f11b33ff4589d5
venkatasiva-narra <75665980+venkatasiva-narra@users.noreply.github.com>
2021-06-08 10:05:19 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -9,8 +9,9 @@ echo "Install git"
 yum install -y git
 
 echo "Install Jenkins"
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+     https://pkg.jenkins.io/redhat-stable/jenkins.repo 
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install -y jenkins
 sudo chkconfig jenkins on
 sudo service jenkins start
